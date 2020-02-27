222fdabed8fb04d45f615557a2e5000bde665a49
shalva-an <s.halvaa26@gmail.com>
2020-02-27 11:31:48 -0500

    Done
    14 minutes left from 90 minute timer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  
+    resources :artists, only: [:index, :show, :new, :create]
+    resources :instruments, only: [:index, :show]
+    resources :artist_instruments, only: [:new, :create]
 end
